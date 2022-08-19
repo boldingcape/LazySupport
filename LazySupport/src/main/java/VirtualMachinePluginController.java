cfbd59eb1cc4845bb26870d60a4a578e531efff9
boldingCape <viroychua@gmail.com>
2022-08-20 00:24:22 +0800

    Basic generic class - not complete

---
+++ LazySupport/src/main/java/VirtualMachinePluginController.java
@@ -0,0 +1,6 @@
+public abstract class VirtualMachinePluginController {
+    public abstract void create();
+    public abstract void run();
+    public abstract void stop();
+    public abstract void delete();
+}
