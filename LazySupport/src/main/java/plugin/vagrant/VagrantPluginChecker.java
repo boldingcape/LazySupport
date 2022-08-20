88f255c5fad0f035985de4ad24e6247e05a6002e
boldingCape <viroychua@gmail.com>
2022-08-20 23:41:32 +0800

    Code implementation and test unit rewrite.

--- LazySupport/src/main/java/plugin/vagrant/VagrantPluginChecker.java
+++ LazySupport/src/main/java/plugin/vagrant/VagrantPluginChecker.java
@@ -1,40 +1,40 @@
 package plugin.vagrant;
 
+import console.Query;
 import plugin.generic.VirtualMachinePluginChecker;
 
 import java.io.*;
 
 public class VagrantPluginChecker implements VirtualMachinePluginChecker {
     private boolean status = false;
+    private String[] commands = {"vagrant", "--version"};
+    public String[] getCommands() {
+        return commands;
+    }
+
+    public void setCommands(String[] commands) {
+        this.commands = commands;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
     @Override
     public void run() {
         try {
-            Runtime rt = Runtime.getRuntime();
-            String[] commands = {"vagrant", "--version"};
-            Process proc = rt.exec(commands);
-            proc.waitFor();
-            if (proc.exitValue() == 0){
-                this.status=true;
-            }
-            else {
-                this.status=false;
+            Query q = new Query();
+            q.request(getCommands());
+            if (q.getRequestState() == 0){
+                setStatus(true);
             }
         }
-        catch (IOException e){
-            System.out.println("Command line query for vagrant status failed: " + e.getStackTrace());
-        } catch (InterruptedException e) {
-            throw new RuntimeException("Waiting for response of query failed: " + e.getStackTrace());
+        catch (Exception e){
+            System.out.println("Invalid Command. Wrapped by: " + e);
         }
     }
-
-    @Override
-    public boolean status() {
-        if (status){
-            System.out.println("Vagrant plugin detected -- Successfully");
-            return true;
-        }
-        System.out.println("Vagrant plugin detected -- Failed");
-        return false;
-    }
-
 }
